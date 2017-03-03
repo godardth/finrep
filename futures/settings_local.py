825cdd335f6d3f94f6a1b4bf68fad5578a9fac32
Theo Godard <theo.godard@gmail.com>
2017-03-03 02:36:10 +0000

    Change credentials to heroku ones

--- futures/settings_local.py
+++ futures/settings_local.py
@@ -1,10 +1,10 @@
 databases = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'finrep',
-        'USER': 'finrep',
-        'PASSWORD': 'finrep',
-        'HOST': 'localhost',
-        'PORT': '',
+        'NAME': 'd2jqni32eifgbe',
+        'USER': 'emchnmszzqkxtz',
+        'PASSWORD': 'b71009be18fe4dfc41ff67844dee37308a7f26a58a7d091910aa664ca985c067',
+        'HOST': 'ec2-54-204-0-88.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
