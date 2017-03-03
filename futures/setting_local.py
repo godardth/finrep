3619cfe4cf87e146f38b1c739f17dbd6c2be72f1
Theo Godard <theo.godard@gmail.com>
2017-03-03 02:13:30 +0000

    Switch to PostgreSQL database

---
+++ futures/setting_local.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'finrep',
+        'USER': 'finrep',
+        'PASSWORD': 'finrep',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
--- futures/settings.py
+++ futures/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import settings_local
+import dj_database_url
+import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,13 +79,11 @@ WSGI_APPLICATION = 'futures.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
+AUTH_USER_MODEL = 'core.User'
+DATABASES = settings_local.databases
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
